b0a81505c3dbba592536199e6b46fce990032111
Gert Drapers <gert.drapers@live.com>
2017-03-04 22:31:45 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
+
+var app = express();
+var inMaintenance = true;
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to write to server.log');
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  if (inMaintenance) {
+    res.render('maintenance.hbs');
+  } else {
+    next();
+  }
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    welcomeMsg: 'Welcome to our page',
+    pageTitle: 'Home Page'
+  });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'page not found'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`server http://localhost:${port}`);
+});
